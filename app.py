07103c382dfc62c56419189316cf3c8b578d38e7
sincosXcom <sincosxcom@gmail.com>
2026-09-15 01:15:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -471,6 +471,6 @@ if os.path.exists(report_file):
     with open(report_file, "r", encoding="utf-8") as f:
         html_content = f.read()
     # 注意：这里可以把 height 调大一点，例如 900 或 1000，避免内部太挤
-    st.components.v1.html(html_content, height=1000, scrolling=True)
+    st.components.v1.html(html_content, height=1500, scrolling=True)
 else:
     st.error(f"未找到报告文件：{report_file}，请先生成该文件。")
